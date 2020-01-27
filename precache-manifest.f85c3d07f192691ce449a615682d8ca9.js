6d354ed9d8c052dd40a8ffa10328f54b243abfa7
miao <ceshigongzhu@gmail.com>
2020-01-27 15:37:46 +0800

    建立pages

---
+++ precache-manifest.f85c3d07f192691ce449a615682d8ca9.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f30c966d102ac7927f",
+    "url": "/mynovel/css/app.dab26ad7.css"
+  },
+  {
+    "revision": "c0c89d80cdd8d26fbf0819ff1ed23bde",
+    "url": "/mynovel/img/ChapterList.png"
+  },
+  {
+    "revision": "965d40542788a1892dea5edb6a942e59",
+    "url": "/mynovel/img/SearchBook.png"
+  },
+  {
+    "revision": "463fc801a7db286858b9a11450cde788",
+    "url": "/mynovel/img/back.png"
+  },
+  {
+    "revision": "032225412c064e4803f90fd620fb9014",
+    "url": "/mynovel/img/bookshelf.png"
+  },
+  {
+    "revision": "d81743154af6d1cffd6ca90e55f17cf1",
+    "url": "/mynovel/img/cancel.png"
+  },
+  {
+    "revision": "15d8f93e519d83118bad084a6939c744",
+    "url": "/mynovel/img/change.png"
+  },
+  {
+    "revision": "43355bd9237107792d4fdbb6f46b9e51",
+    "url": "/mynovel/img/circle.png"
+  },
+  {
+    "revision": "d1480b3a5f4be987d14dd1d0d9827174",
+    "url": "/mynovel/img/clock.png"
+  },
+  {
+    "revision": "48a04f5517eb491d9582234755bead7a",
+    "url": "/mynovel/img/home.48a04f55.png"
+  },
+  {
+    "revision": "48a04f5517eb491d9582234755bead7a",
+    "url": "/mynovel/img/home.png"
+  },
+  {
+    "revision": "b7b36eb5b580854253add233b42b7abb",
+    "url": "/mynovel/img/list.png"
+  },
+  {
+    "revision": "b1d94bb3c3c95c56ff07cd9a3092f600",
+    "url": "/mynovel/img/loading-book.b1d94bb3.gif"
+  },
+  {
+    "revision": "b1d94bb3c3c95c56ff07cd9a3092f600",
+    "url": "/mynovel/img/loading-book.gif"
+  },
+  {
+    "revision": "563b474ad452cdba170f39e2d8101917",
+    "url": "/mynovel/img/loading.gif"
+  },
+  {
+    "revision": "f4f29b2f3799587dcf902af794c57b92",
+    "url": "/mynovel/img/logo.f4f29b2f.png"
+  },
+  {
+    "revision": "f4f29b2f3799587dcf902af794c57b92",
+    "url": "/mynovel/img/logo.png"
+  },
+  {
+    "revision": "1b665a8ef8eb62b247e305d2fe668aa5",
+    "url": "/mynovel/img/longtext.png"
+  },
+  {
+    "revision": "288c2a3dd9beeef28d34bd0d86c78f55",
+    "url": "/mynovel/img/ncon.288c2a3d.png"
+  },
+  {
+    "revision": "288c2a3dd9beeef28d34bd0d86c78f55",
+    "url": "/mynovel/img/ncon.png"
+  },
+  {
+    "revision": "a9b930eb87538bc67b8861d8e717b724",
+    "url": "/mynovel/img/rcon.a9b930eb.png"
+  },
+  {
+    "revision": "a9b930eb87538bc67b8861d8e717b724",
+    "url": "/mynovel/img/rcon.png"
+  },
+  {
+    "revision": "7e954279c7b2602131c301d7fc4cfbd1",
+    "url": "/mynovel/img/readicon.png"
+  },
+  {
+    "revision": "4e18a23d0b21f9e2b1ac0f124fa03db3",
+    "url": "/mynovel/img/search.png"
+  },
+  {
+    "revision": "4dc2c17e83cb497d796b231c54e61a69",
+    "url": "/mynovel/index.html"
+  },
+  {
+    "revision": "16f30c966d102ac7927f",
+    "url": "/mynovel/js/app.6e29d7b2.js"
+  },
+  {
+    "revision": "76407f0008d641257096",
+    "url": "/mynovel/js/chunk-vendors.5eff4dee.js"
+  },
+  {
+    "revision": "a5a9a3ece3013985cbf4b0ae892d53df",
+    "url": "/mynovel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mynovel/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mynovel/precache-manifest.f85c3d07f192691ce449a615682d8ca9.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "novel"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
